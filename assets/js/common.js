18846e638bbe41d0117ab1032f8630c6ec9ff3ca
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-02-20 21:31:30 +0300

    carousel

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,23 +1,56 @@
 
  $(document).ready(function() {
-            $("#my-menu").mmenu({
-                extensions: ['effect-menu-slide', 'pagedim-black', "fx-listitems-fade", 'position-right'],
-                navbar: {
-                    //title: '<img src="/assets/img/main/logo-1.png">'
-                },
-            }, {
-                // configuration
-                offCanvas: {
-                    pageSelector: "#my-page"
-                }
-            });
+    $("#my-menu").mmenu({
+        extensions: ['effect-menu-slide', 'pagedim-black', "fx-listitems-fade", 'position-right'],
+        navbar: {
+            //title: '<img src="/assets/img/main/logo-1.png">'
+        },
+    }, {
+        // configuration
+        offCanvas: {
+            pageSelector: "#my-page"
+        }
+    });
 
-            var api = $('#my-menu').data('mmenu');
-            api.bind('open:start', function() {
-                $('.hamburger').addClass('is-active');
-            });
-            api.bind('close:before', function() {
-                $('.hamburger').removeClass('is-active');
-            })
-        });
+    var api = $('#my-menu').data('mmenu');
+    api.bind('open:start', function() {
+        $('.hamburger').addClass('is-active');
+    });
+    api.bind('close:before', function() {
+        $('.hamburger').removeClass('is-active');
+    })
+});
 
+$(document).ready(function(){
+    $('.recipes-carousel').owlCarousel({
+        loop:true,
+        margin:15,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+                dots:false,
+                nav:false
+            },
+            400:{
+                items:2,
+                dots:false,
+                nav:false
+            },
+            600:{
+                items:3,
+                dots:false,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:false,
+                dots:false,
+                loop:true
+            }
+        }
+    });
+})
